a9dedf19349fecf220e9b5e0a8580837c9687722
datchlive <71320906+DatchLive@users.noreply.github.com>
2022-02-17 16:41:34 +0900

    ✨ メモ一覧表示実装

--- front/pages/memos/index.tsx
+++ front/pages/memos/index.tsx
@@ -1,42 +1,31 @@
+import { AxiosError, AxiosResponse } from 'axios';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { axiosApi } from '../../lib/axios';
 
 type Memo = {
   title: string;
   body: string;
 };
 
-const tempMemos: Memo[] = [
-  {
-    title: '仮のタイトル1',
-    body: '仮のメモの内容1',
-  },
-  {
-    title: '仮のタイトル2',
-    body: '仮のメモの内容2',
-  },
-  {
-    title: '仮のタイトル3',
-    body: '仮のメモの内容3',
-  },
-  {
-    title: '仮のタイトル4',
-    body: '仮のメモの内容4',
-  },
-  {
-    title: '仮のタイトル5',
-    body: '仮のメモの内容5',
-  },
-];
-
 const Memo: NextPage = () => {
   const router = useRouter();
+
+  const [memos, setMemos] = useState<Memo[]>([]);
+
+  useEffect(() => {
+    axiosApi
+      .get('/api/memos')
+      .then((response: AxiosResponse) => {console.log(response.data); setMemos(response.data.data);})
+      .catch((err: AxiosError) => console.log(err.response));
+  }, []);
 
   return (
     <div className='w-2/3 mx-auto mt-32'>
       <div className='w-1/2 mx-auto text-center'>
         <button
-          className='text-xl mb-12 py-3 px-10 bg-blue-500 text-white rounded-3xl drop-shadow-md hover:bg-blue-400'
+          className='px-10 py-3 mb-12 text-xl text-white bg-blue-500 rounded-3xl drop-shadow-md hover:bg-blue-400'
           onClick={() => router.push('/memos/post')}
         >
           メモを追加する
@@ -44,11 +33,11 @@ const Memo: NextPage = () => {
       </div>
       <div className='mt-3'>
         {/* 仮データでの一覧表示 */}
-        <div className='grid w-2/3 mx-auto gap-4 grid-cols-2'>
-          {tempMemos.map((memo: Memo, index) => {
+        <div className='grid w-2/3 grid-cols-2 gap-4 mx-auto'>
+          {memos.map((memo: Memo, index) => {
             return (
-              <div className='bg-gray-100 shadow-lg mb-5 p-4' key={index}>
-                <p className='text-lg font-bold mb-1'>{memo.title}</p>
+              <div className='p-4 mb-5 bg-gray-100 shadow-lg' key={index}>
+                <p className='mb-1 text-lg font-bold'>{memo.title}</p>
                 <p className=''>{memo.body}</p>
               </div>
             );
